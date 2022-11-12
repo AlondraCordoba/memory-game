c16e2e11784a745e329757ce96cf7def06c5850d
AlondraCordoba <alondracordoba0720 @gmail.com>
2022-11-11 19:22:54 -0600

    fix(error): Error fixed, some advance

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,19 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button");
 
+let maxTime = 20;
+let timeLeft = maxTime;
+let flips = 0;
 let matched = 0;
-let firstCard, secondCard;
 let disabled = false;
+let isPlaying = false;
+let firstCard, secondCard, timer;
 
 function cardFlipped({target: cardSelected}) {
+    if(!isPlaying) {
+        isPlaying = true;
+    }
     if(firstCard !== cardSelected && !disabled) {
         cardSelected.classList.add("flip");
         if(!firstCard) {
@@ -21,10 +30,8 @@ function cardFlipped({target: cardSelected}) {
 function cardsMatched(img1, img2) {
     if(img1 === img2) {
         matched++;
-        if(matched == 8) {
-            setTimeout(() => {
-                return cardRandom();
-            }, 1000);
+        if(matched == 8 && timeLeft > 0) {
+            return clearInterval(timer);
         }
         firstCard.removeEventListener("click", cardFlipped);
         secondCard.removeEventListener("click", cardFlipped);
@@ -36,6 +43,8 @@ function cardsMatched(img1, img2) {
     setTimeout(() => {
         firstCard.classList.add("shake");
         secondCard.classList.add("shake");
+        flips++;
+        flipsTag.innerText = flips;
     }, 400);
 
     setTimeout(() => {
@@ -47,20 +56,29 @@ function cardsMatched(img1, img2) {
 }
 
 function cardRandom() {
-    matched = 0;
-    disabled = false;
+    timeLeft = maxTime;
+    flips = matched = 0;
     firstCard = secondCard = "";
+    clearInterval(timer);
+    flipsTag.innerText = flips;
+    disabled = isPlaying = false;
+
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    arr.sort(() => Math.round(Math.random()) - 0.5);
+
     cards.forEach((card, i) => {
         card.classList.remove("flip", "matched");
         let imgTag = card.querySelector(".back-card img");
-        imgTag.src = `assets/img/icon-${arr[i]}.png`;
+        setTimeout(() => {
+            imgTag.src = `assets/img/icon-${arr[i]}.png`;
+        }, 500);
         card.addEventListener("click", cardFlipped);
     });
 }
 
 cardRandom();
+
+refreshBtn.addEventListener("click", cardRandom);
 
 cards.forEach(card => {
     card.addEventListener("click", cardFlipped);
